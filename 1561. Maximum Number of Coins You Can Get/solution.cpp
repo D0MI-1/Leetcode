44270cebf7e8badc170ea8e12c830a4b86c2c70f
Dominik Urban <dominik.urban1@gmx.de>
2023-11-25 20:17:13 +0100

    1685 solved

--- 1561. Maximum Number of Coins You Can Get/solution.cpp
+++ 1561. Maximum Number of Coins You Can Get/solution.cpp
@@ -8,19 +8,18 @@ public:
     int maxCoins(std::vector<int>& piles) {
         std::vector<int>& tmp = piles; 
         int solution = 0;
-
-        std::sort(piles.begin(), piles.end(), std::greater<int>());
-        
         auto it = piles.begin();
         auto n = piles.size() / 3;
 
+        std::sort(piles.begin(), piles.end(), std::greater<int>());
+        
+
         for (size_t i = 0; i < n; i++)
         {
-            //Go to second Highest
             std::advance(it, 2);
-            
             solution += *it;
-            //Skip 3
+
+            // Skip one element (lowest value)
             ++it;
         }
         
