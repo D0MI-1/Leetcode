2701b2a0c338f26db0a40e4b3fdc3cd7797a5aa2
Dominik Urban <dominik.urban1@gmx.de>
2023-12-08 10:37:15 +0100

    606 solved

--- 1903. Largest Odd Number in String/solution.cpp
+++ 1903. Largest Odd Number in String/solution.cpp
@@ -4,6 +4,7 @@ class Solution {
 public:
     std::string largestOddNumber(std::string num) {
         for (int i = num.size() - 1; i >= 0; --i) {
+            //subtracting '0' converts the character '0' to the integer 0, '1' to 1
             if ((num[i] - '0') % 2 != 0) {
                 return num.substr(0, i + 1);
             }
---
+++ 606. Construct String from Binary Tree/solution.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include "solution.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    std::string tree2str(TreeNode* root) {
+        if (root == nullptr) {
+            return ""; 
+        }
+
+        std::string result = std::to_string(root->val);
+
+        if (root->left != nullptr || root->right != nullptr) {
+            result += "(" + tree2str(root->left) + ")";
+        }
+
+        if (root->right != nullptr) {
+            result += "(" + tree2str(root->right) + ")";
+        } 
+
+        return result;
+    }
+};
