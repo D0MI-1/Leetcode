85ed9ea6adc87082110984e7d8fff85f90dab065
Dominik Urban <dominik.urban1@gmx.de>
2024-01-03 00:12:19 +0100

    2610 solved

--- 2610. Convert an Array Into a 2D Array With Conditions/solution.cpp
+++ 2610. Convert an Array Into a 2D Array With Conditions/solution.cpp
@@ -1,6 +1,52 @@
+#include <vector>
+#include <array>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> findMatrix(std::vector<int>& nums) {
+        std::vector<std::vector<int>> solution;
+
+        const int size = 201;
+
+        std::array<int, size> numsCounter = {0};
+
+        for (int num : nums){
+            int index = numsCounter[num];
+
+            if (index == solution.size()) {
+                solution.push_back({});
+            }
+
+            solution[index].push_back(num);
+
+            numsCounter[num]++;
+        }
+
+        return solution;
+    }
+};
+
+/*
+
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
+        vector<int> frequency(nums.size() + 1);
         
+        vector<vector<int>> result;
+        for (int num : nums) {
+            if (frequency[num] >= result.size()) {
+                result.push_back({});
+            }
+            
+            
+            result[frequency[num]].push_back(num);
+            frequency[num]++;
+        }
+        
+        return result;
     }
 };
+
+
+*/
