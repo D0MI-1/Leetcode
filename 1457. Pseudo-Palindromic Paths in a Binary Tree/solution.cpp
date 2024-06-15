31d5d8a7dbc44bfdbc40f220df33b51131a7e84f
Dominik Urban <dominik.urban1@gmx.de>
2024-06-15 07:37:19 +0200

    1457 solved

--- 1457. Pseudo-Palindromic Paths in a Binary Tree/solution.cpp
+++ 1457. Pseudo-Palindromic Paths in a Binary Tree/solution.cpp
@@ -13,29 +13,37 @@
 #include "solution.h"
 
 class Solution {
-    int result;
+    int result; // This variable will store the final result.
 public:
+    // Helper function to check if a number is a power of two.
     bool isPowerOfTwo(int n){
         return n == 0 ? true : !(n&(n-1));
     }
 
+    // DFS function to traverse the binary tree and count pseudo-palindromic paths.
     void dfs(TreeNode* root, int value){
-        if(root == 0)return;
+        if(root == 0) return;
 
+        // XOR the current value with 1 shifted to the left by the root's value.
         value ^= 1 << root->val;
 
+        // Check if the current node is a leaf (no left and right children).
         if(root->left == root->right){
+            // If the XOR value has at most one '1' bit, it is a pseudo-palindromic path.
             result += static_cast<int>(isPowerOfTwo(value));
         }
         else {
+            // Recursively call DFS for the left and right children.
             dfs(root->left, value);
             dfs(root->right, value);
         }
     }
-    int pseudoPalindromicPaths (TreeNode* root) {
+
+    // Main function to calculate pseudo-palindromic paths in the binary tree.
+    int pseudoPalindromicPaths(TreeNode* root) {
         result = 0;
         dfs(root, 0);
-        root->left = root->right = 0;
+        root->left = root->right = 0; // This line seems to be an attempt to free memory, but it may cause issues.
         return result;
     }
 };
